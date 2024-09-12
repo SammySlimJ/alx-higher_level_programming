805794c70b59a6a681981e1339b7d54f247bcbb9
SammySlimJ <samanthasalimu@gmail.com>
2024-09-12 11:04:56 -0700

    took off the result declaration

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ if _name_ == "_main_":
     from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} + {}".format(a, b, result))
+    add(a, b)
+    print("{} + {} + {}".format(a, b, add(a, b)))
