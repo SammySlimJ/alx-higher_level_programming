ca8e1e65afc1b1dbd8866c45bcffc67b221ffb0e
SammySlimJ <samanthasalimu@gmail.com>
2024-10-02 10:09:12 -0700

    Implement Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+
+The Square class is a simple representation of a geometric square.
+"""
+
+
+class Square:
+    """
+    Square class that represents a square.
+
+    This class currently does not have any attributes or methods.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This README.md is for the topic of Classes and Objects in Python
